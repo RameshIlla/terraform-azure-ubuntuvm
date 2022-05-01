263996718cfa4ebac92fb0db1e888ed34390dfbd
Ramesh msft <v-rameshilla@microsoft.com>
2022-05-01 14:02:34 +0800

    Added var.tf

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "location"
+{
+    type =  string
+    default = "Singapore"
+}
+variable "prefix"
+{
+    type = string
+    default="demoprefix"
+}
+variable "ssh-source-address" 
+{
+    type=string
+    default="*"
+}
