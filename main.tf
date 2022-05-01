a24eb97ed53af726d389cfb23a761585f093a2e6
Ramesh msft <v-rameshilla@microsoft.com>
2022-05-01 14:04:16 +0800

    corrected block definitions

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-provider "azurerm"
-{
+provider "azurerm"{
     version = "2.90.0"
 }
 
 #Create a resource group
-resource "azurerm_resource_group" "vm resource group"
-{
+resource "azurerm_resource_group" "vm resource group"{
     name = "vm-resource-group"
     location = var.location
 }
--- var.tf
+++ var.tf
@@ -1,15 +1,12 @@
-variable "location"
-{
+variable "location"{
     type =  string
     default = "Singapore"
 }
-variable "prefix"
-{
+variable "prefix"{
     type = string
     default="demoprefix"
 }
-variable "ssh-source-address" 
-{
+variable "ssh-source-address" {
     type=string
     default="*"
 }
